ad0c59afe4b13f86f8d2a53f3a3461908be26aaa
Peter Szekeli <p.szekeli@p1group.nl>
2022-11-30 14:03:39 +0100

    feat: cleanup

--- API/Controllers/FlightsController.cs
+++ API/Controllers/FlightsController.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using API.ApiResponses;
-using Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -11,16 +10,10 @@ namespace API.Controllers;
 [Route("[controller]")]
 public class FlightsController : ControllerBase
 {
-    private readonly FlightsContext _context;
-
-    public FlightsController(FlightsContext context)
-    {
-        _context = context;
-    }
 
     [HttpGet]
     [Route("Search")]
-    public async Task<IEnumerable<FlightResponse>> GetAvailableFlights()
+    public Task<IEnumerable<FlightResponse>> GetAvailableFlights()
     {
         throw new NotImplementedException();
     }
